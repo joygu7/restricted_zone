61a2e395b3f1978c77406c3ba41efdcf2dd9a1c1
joygu7 <joygu7>
2022-10-05 12:16:00 -0500

    added warning zone flag, offset for bounding box centroid, and fixed frame display

--- projection.py
+++ projection.py
@@ -8,17 +8,21 @@ import numpy as np
 
 # looks like there's a bit of camera drift as the video continues as the zone line shifts slightly
 
+# May want a separate calibration/setup procedure to select source/destination points and zone boundary
+
 # configurations
 frame_rate = 60.0  # frames per second
 video_name = 'worker-zone-detection.mp4'  # taken from https://github.com/intel-iot-devkit/sample-videos
+boundingbox_shift = -50  # number of pixels to y coordinate of centroid to match with foot location
 
 
 # define restricted zone - this should be defined in transformed perspective coordinate system
 class RestrictedZone:
     def __init__(self, pts):
-        self.pts = pts  # points for border of polygon
+        self.pts = pts  # points for border of polygon - should be in order of how to connect
         self.color = (0, 0, 255)  # BGR
         self.alpha = 0.4  # Transparency factor
+        self.warning_dist = 15  # pixel distance from edge to create a warning zone
 
     # overlay the zone on the image with some transparency
     def overlay_image(self, img):
@@ -28,12 +32,18 @@ class RestrictedZone:
         return image_overlaid
 
     # determine if the point is within the zone
+    # Return True if in the zone, False if not in zone, otherwise return string 'Warning Zone'
     def is_point_in_zone(self, test_pt):
-        dist = cv2.pointPolygonTest(self.pts, test_pt, False)
+        dist = cv2.pointPolygonTest(self.pts, test_pt, True)
+        # if True parameter, it will
+        # return the distance from the edge, could be useful for defining a buffer zone at the edges
         if dist >= 0:  # positive or at border 0 means inside the polygon
             return True
         else:
-            return False
+            if abs(dist) <= self.warning_dist:  # if the centroid distance from edge is within warning distance
+                return "Warning Zone"
+            else:
+                return False
 
 
 # Defining a projection class
@@ -85,7 +95,8 @@ print(my_transform.matrix)
 
 # set up output video
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('transformed_output.mp4', fourcc, frame_rate, my_transform.size)
+out = cv2.VideoWriter('overlay_output.mp4', fourcc, frame_rate, my_transform.size)
+out_tf = cv2.VideoWriter('transformed_output.mp4', fourcc, frame_rate, my_transform.size)
 
 # person detector - fill in with backpack later
 # initialize the HOG descriptor/person detector
@@ -117,32 +128,35 @@ while success:  # loop through all the frames
             # display the detected boxes in the colour picture
             # cv2.rectangle(img_resized, (xA, yA), (xB, yB),
             #               (0, 255, 0), 2)
-            centroid = (int((xA + xB) / 2), int(yB))  # centroid in original
+            centroid = (int((xA + xB) / 2), int(yB) + boundingbox_shift)  # centroid in original
             transformed_centroid = my_transform.transform_pt(centroid)  # centroid in new coordinate system
             # print(transformed_centroid)
             zone_flag = red_zone.is_point_in_zone(transformed_centroid)
-            # cv2.drawMarker(img_resized, centroid, (255,0,0),thickness=2)
+            cv2.drawMarker(img_resized, centroid, (255,0,0),thickness=2)
             cv2.drawMarker(image_new, transformed_centroid, (255, 0, 0), thickness=2)
 
             # add some text next to centroid whether it's in the zone or not
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
             thickness = 2
-            if zone_flag:
+            if zone_flag == "Warning Zone":
+                color = (255, 255, 0)
+            elif zone_flag:
                 color = (255, 0, 0)
             else:
                 color = (0, 255, 0)
             cv2.putText(image_new, str(zone_flag), (transformed_centroid[0] + 2, transformed_centroid[1] + 2), font,
                         fontScale, color, thickness, cv2.LINE_AA)
+            cv2.putText(img_resized, str(zone_flag), (centroid[0] + 2, centroid[1] + 2), font,
+                        fontScale, color, thickness, cv2.LINE_AA)
 
         # write the output frame to video
-        # out.write(tf_img)
-        out.write(image_new)
-        # out.write(img_resized)
+        out_tf.write(image_new)
+        out.write(img_resized)
 
-        # cv2.imshow(str(counter), image_new)  # Transformed Capture
-        # cv2.waitKey(200)
-        # cv2.destroyAllWindows()
+        cv2.imshow('transformed', image_new)  # Transformed Capture
+        cv2.imshow('original', img_resized)  # Transformed Capture
+        cv2.waitKey(1)
 
     # read the next frame
     success, image = vidcap.read()
@@ -151,3 +165,4 @@ while success:  # loop through all the frames
 # end loop, release objects
 out.release()
 vidcap.release()
+cv2.destroyAllWindows()
